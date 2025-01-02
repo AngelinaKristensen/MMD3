e5fa5e1c891f25ebfd12ed36f4aeb0f01d517d1c
AngeKri <10511638@ucn.dk>
2025-01-02 11:24:26 +0100

    Basic nav setup

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -1,11 +1,47 @@
 <script setup>
-
+import { RouterLink } from 'vue-router';
 </script>
 
 <template>
-
+ <nav>
+    <ul>
+        <li><RouterLink to="/">Forside</RouterLink></li>
+        <li><RouterLink to="/VoresOel">Vores Øl</RouterLink></li>
+        <li>
+            <button>Bryghus</button>
+            <div>
+                <RouterLink to="/Bryggerstuen">Bryggerstuen</RouterLink>
+                <RouterLink to="/Brygproces">Brygproces</RouterLink>
+                <RouterLink to="/SmagsGuide">Smags Guide</RouterLink>
+                <RouterLink to="/Rundvisning">Rundvisning</RouterLink>
+                <RouterLink to="/Vaerdier">Værdier</RouterLink>
+            </div>
+        </li>
+        <li><RouterLink to="/Begivenheder">Begivenheder</RouterLink></li>
+        <li><RouterLink to="/Kontakt">Kontakt</RouterLink></li>
+        <li><RouterLink to="/Galleri">Galleri</RouterLink></li>
+    </ul>
+ </nav>
 </template>
 
 <style scoped>
-
+nav ul{
+    background-color: grey;
+    display: flex;
+    justify-content: center;
+    padding: 2rem;
+    gap: 4rem;
+    margin: 0;
+}
+nav li{
+    list-style: none;
+    font-family: "Exo", sans-serif;
+    font-weight: 600;
+}
+nav a{
+    text-decoration: none;
+    text-transform: uppercase;
+    color: white;
+    position: relative;
+}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,92 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
+import HomeView from '@/views/HomeView.vue'
+import BeerView from '@/views/BeerView.vue'
+import BryggerstuenView from '@/views/BryggerstuenView.vue'
+import BrewingProcessView from '@/views/BrewingProcessView.vue'
+import TasteGuideView from '@/views/TasteGuideView.vue'
+import TourView from '@/views/TourView.vue'
+import ValuesView from '@/views/ValuesView.vue'
+import EventsView from '@/views/EventsView.vue'
+import ContactView from '@/views/ContactView.vue'
+import GalleriView from '@/views/GalleriView.vue'
+
+// const router = createRouter({
+//   history: createWebHashHistory(import.meta.env.BASE_URL),
+//   routes: [
+//     {
+//       path: '/',
+//       name: 'home',
+//       component: HomeView,
+//     },
+//     {
+//       path: '/about',
+//       name: 'about',
+//       // route level code-splitting
+//       // this generates a separate chunk (About.[hash].js) for this route
+//       // which is lazy-loaded when the route is visited.
+//       component: () => import('../views/ContactView.vue'),
+//     },
+//   ],
+// })
+
+const routes = [
+  {
+    path: "/",
+    name: "forside",
+    component: HomeView
+  },
+  {
+    path: "/VoresOel",
+    name: "voresoel",
+    component: BeerView
+  },
+  {
+    path: "/Bryggerstuen",
+    name: "bryggerstuen",
+    component: BryggerstuenView
+  },
+  {
+    path: "/Brygproces",
+    name: "brygproces",
+    component: BrewingProcessView
+  },
+  {
+    path: "/SmagsGuide",
+    name: "smagsguide",
+    component: TasteGuideView
+  },
+  {
+    path: "/Rundvisning",
+    name: "rundvisning",
+    component: TourView
+  },
+  {
+    path: "/Vaerdier",
+    name: "vaerdier",
+    component: ValuesView
+  },
+  {
+    path: "/Begivenheder",
+    name: "begivenheder",
+    component: EventsView
+  },
+  {
+    path: "/Kontakt",
+    name: "kontakt",
+    component: ContactView
+  },
+  {
+    path: "/Galleri",
+    name: "galleri",
+    component: GalleriView
+  },
+];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-  ],
-})
+  history: createWebHashHistory(import.meta.env.BASE_URL),
+  routes
+});
+
+
 
 export default router
