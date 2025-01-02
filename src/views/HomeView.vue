0274b54b59f52708bb9fc0994c58dcc134dea898
Mohamed <57219435+Posedion1920@users.noreply.github.com>
2025-01-02 12:26:19 +0100

    wwe

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,9 +3,21 @@
 </script>
 
 <template>
-
+    <header>
+        <picture>
+            <img src="../assets/billeder/logodesktop.png" alt="">
+            <source srcset="../assets/billeder/logomobil.png" media="(max-width: 430px)">
+            <source srcset="../assets/billeder/logodesktop.png" media="(min-width: 1000px)">
+            <img src="../assets/billeder/logodesktop.png" alt="løkken bryghus loo">
+        </picture>
+    </header>
 </template>
 
 <style scoped>
 
+@media (max-width: 430px){
+    header{
+        background-image: url(../assets/billeder/);
+    }
+}
 </style>
