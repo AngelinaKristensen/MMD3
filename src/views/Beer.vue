5378ec529f14df6dcbf2cbb01ec6b9c90002c9c9
Mohamed <57219435+Posedion1920@users.noreply.github.com>
2025-01-03 23:14:21 +0100

    beersite

---
+++ src/views/Beer.vue
@@ -0,0 +1,114 @@
+<script setup>
+
+</script>
+
+<template>
+    <main>
+        <section class="sec">
+            <img src="../assets/billeder/BadenymfenWithBeer 2.png" alt="" class="beercenter">
+            <article class="boxmargin">
+                <div>
+                    <h1>Badenymfen</h1>
+                    <h3>Fruit Beer</h3>
+                    <p class="text">Badenymfen er brygget med valle fra Ingstrup mejeri. Et overskud fra osteproduktion. Den er let og sommerlig.</p>
+                </div>
+                <div class="infoContainer">
+                    <hr class="line">
+                    <div class="info">
+                       <div class="icon"> <img src="../assets/billeder/type.svg" alt=""></div>
+                       <p>Type: Frugt Øl</p>
+                    </div>
+                    <hr class="line">
+                    <div class="info">
+                       <div class="icon"> <img src="../assets/billeder/type.svg" alt=""></div>
+                       <p>Type: Frugt Øl</p>
+                    </div>
+                    <hr class="line">
+                    <div class="info">
+                       <div class="icon"> <img src="../assets/billeder/type.svg" alt=""></div>
+                       <p>Type: Frugt Øl</p>
+                    </div>
+                    <hr class="line">
+                    <div class="info">
+                       <div class="icon"> <img src="../assets/billeder/type.svg" alt=""></div>
+                       <p>Type: Frugt Øl</p>
+                    </div>
+                    <hr class="line">
+                    <div class="info">
+                       <div class="icon"> <img src="../assets/billeder/type.svg" alt=""></div>
+                       <p>Type: Frugt Øl</p>
+                    </div>
+                    <hr class="line">
+                </div>
+            </article>
+        </section>
+    </main>
+</template>
+
+<style scoped>
+@media (min-width:1000px) {
+    .text{
+    font-size: 20px;
+    width: 40ch;
+    margin: 36px 0px 46px 0px;
+}
+
+h1{
+    font-size: 64px;
+    margin-bottom: 0px;
+}
+h3{
+    font-size: 32px;
+}
+.sec{
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    align-items: center;
+    max-width: 1200px;
+    margin: 0 auto;
+    column-gap: 2rem;
+}
+.sec img{
+    grid-column: 2;
+    display: block;
+    margin: 0;
+}
+
+.sec article{
+    grid-column: 1;
+    grid-row: 1;
+}
+}
+
+.beercenter{
+    display: block;
+    margin: 0 auto;
+}
+.boxmargin {
+    margin: 0rem 24px;
+}
+.infoContainer{
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+}
+
+.line{
+    height: 3px;
+    background-color: black;
+}
+.info{
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+}
+.icon{
+    width: 40px;
+    height: 40px;
+    background-color: var(--secondary);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50%;
+}
+</style>
