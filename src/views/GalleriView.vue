e67d8ba2f8cc31e390034ea91df836f3e6c2b5f8
Mohamed <57219435+Posedion1920@users.noreply.github.com>
2025-01-08 15:45:34 +0100

    galleri færdig

--- src/views/GalleriView.vue
+++ src/views/GalleriView.vue
@@ -4,7 +4,54 @@
 
 <template>
     <section>
-        
+        <picture>
+            <source srcset="../assets/billeder/Galleri1Desktop.png" media="(min-width: 1000px)">
+            <img src="../assets/billeder/Galleri1Mobil.png" alt="">
+        </picture>
+        <picture>
+            <source srcset="../assets/billeder/Galleri2Desktop.png" media="(min-width: 1000px)">
+            <img src="../assets/billeder/Galleri2Mobil.png" alt="">
+        </picture>
+        <picture>
+            <source srcset="../assets/billeder/Galleri3Desktop.png" media="(min-width: 1000px)">
+            <img src="../assets/billeder/Galleri3Mobil.png" alt="">
+        </picture>
+        <picture>
+            <source srcset="../assets/billeder/Galleri4Desktop.png" media="(min-width: 1000px)">
+            <img src="../assets/billeder/Galleri4Mobil.png" alt="">
+        </picture>
+        <picture>
+            <source srcset="../assets/billeder/Galleri5Desktop.png" media="(min-width: 1000px)">
+            <img src="../assets/billeder/Galleri5Mobil.png" alt="">
+        </picture>
+        <picture>
+            <source srcset="../assets/billeder/Galleri6Desktop.png" media="(min-width: 1000px)">
+            <img src="../assets/billeder/Galleri6Mobil.png" alt="">
+        </picture>
+        <picture>
+            <source srcset="../assets/billeder/Galleri7Desktop.png" media="(min-width: 1000px)">
+            <img src="../assets/billeder/Galleri7Mobil.png" alt="">
+        </picture>
+        <picture>
+            <source srcset="../assets/billeder/Galleri8Desktop.png" media="(min-width: 1000px)">
+            <img src="../assets/billeder/Galleri8Mobil.png" alt="">
+        </picture>
+        <picture>
+            <source srcset="../assets/billeder/Galleri9Desktop.png" media="(min-width: 1000px)">
+            <img src="../assets/billeder/Galleri9Mobil.png" alt="">
+        </picture>
+        <picture>
+            <source srcset="../assets/billeder/Galleri10Desktop.png" media="(min-width: 1000px)">
+            <img src="../assets/billeder/Galleri10Mobil.png" alt="">
+        </picture>
+        <picture>
+            <source srcset="../assets/billeder/Galleri11Desktop.png" media="(min-width: 1000px)">
+            <img src="../assets/billeder/Galleri11Mobil.png" alt="">
+        </picture>
+        <picture>
+            <source srcset="../assets/billeder/Galleri12Desktop.png" media="(min-width: 1000px)">
+            <img src="../assets/billeder/Galleri12Mobil.png" alt="">
+        </picture>
     </section>
 </template>
 
