818546d6ab1e00d24f463b051a8c29f4cafe7a34
Mohamed <57219435+Posedion1920@users.noreply.github.com>
2025-01-09 15:17:40 +0100

    wqeq

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -26,9 +26,6 @@ const showRest = () => {
         <div class="timendPlace"><p>Fredag 12-19 Lørdag 11-18</p>
             <p>Vrenstedvej 12</p></div>
        </div>
-       <svg class="arrow" width="79" height="107" viewBox="0 0 79 107" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M28.2348 80.4304C30.3173 82.5232 33.6993 82.5232 35.7819 80.4304L62.4381 53.6432C64.5206 51.5505 64.5206 48.1518 62.4381 46.0591C60.3556 43.9663 56.9736 43.9663 54.8911 46.0591L37.3312 63.7219V12.3574C37.3312 9.39411 34.9488 7 32 7C29.0512 7 26.6687 9.39411 26.6687 12.3574V63.7052L9.10894 46.0758C7.02642 43.9831 3.64441 43.9831 1.56189 46.0758C-0.52063 48.1686 -0.52063 51.5672 1.56189 53.66L28.2181 80.4472L28.2348 80.4304Z" fill="#168039"/>
-</svg>
     </header>
    </div>
     <main>
@@ -85,11 +82,6 @@ const showRest = () => {
 .wrapper2{
     padding: 24px 0px 24px 20px;
 }
-.arrow{
-    position: absolute;
-    bottom: 220px;
-    animation: bounce 2s ease-in-out infinite;
-}
 
 .grid2{
     display: flex;
@@ -121,7 +113,6 @@ header{
     align-items: center;
     justify-content: center;
     color: white;
-    position: relative;
 }
 
 i{
@@ -176,18 +167,6 @@ header>div{
     gap: 12px;
 }
 
-
-@keyframes bounce{
-    0%{
-        transform: translateY(0px) scale(1);
-    }
-    50%{
-        transform: translateY(-30px) scale(1.1);
-    }
-    100%{
-        transform: translateY(0px) scale(1);
-    }
-}
 
 .aligntext{
     width: 21ch;
